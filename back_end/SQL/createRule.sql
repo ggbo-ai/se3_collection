4607949c76c72412a1523a5e8ac92f58c0a21746
ggbo-ai <71574508+ggbo-ai@users.noreply.github.com>
2022-02-22 16:20:50 +0800

    Add files via upload

---
+++ back_end/SQL/createRule.sql
@@ -0,0 +1,8 @@
+drop database if  exists db6;
+create database db6;
+
+
+
+
+
+
---
+++ back_end/src/test/java/com/Collection/VersionHitApplicationTests.java
@@ -0,0 +1,14 @@
+package com.Collection;
+
+import com.Collection.Mapping.HitMapping;
+import com.Collection.Mapping.RuleMapping;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VersionHitApplicationTests {
+
+    @Test
+    void contextLoads() {}
+
+}
